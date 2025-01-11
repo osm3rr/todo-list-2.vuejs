2599215d0c7a218edbe55bbef2b9dbb4c356a8b8
osm3rr <osm3rr@adakademy.com>
2025-01-11 13:26:03 -0400

    to do list done

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,9 +1,21 @@
-<script lang="ts" setup>defineProps<{ isChecked: boolean | false }>()
+<script lang="ts" setup>
+    // const props = defineProps<{ isChecked?: boolean }>();
+    const props = withDefaults(
+        defineProps<{
+            isChecked: boolean
+        }>(),
+        {  isChecked: false
+        }
+    )
 </script>
 
 <template>
-    <label :class="{checked:isChecked}">
-        <input type="checkbox":checked="isChecked">
+    <label :class="{ checked: isChecked }">
+        <input 
+            type="checkbox" 
+            :checked="isChecked"
+            @change="$emit('toggle')"
+            />
         <slot></slot>
     </label>
 </template>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,51 @@
 <script lang="ts" setup>
+    import {ref } from 'vue';
+    import type { Ref } from 'vue';
     import ListItem from './ListItem.vue';
+    type Item = {
+        title: string,
+        checked?: boolean
+    };
+
+    const listItems: Ref<Item[]> = ref([
+        {title: 'Aprender Vue 3', checked: true },
+        {title: 'Hacer ejercicio', checked: false },
+        {title: 'Leer un libro al mes'},
+    ])
+
+    const updateItem = (item: Item): void => {
+        const updatedItem = findItemInList(item) 
+        if (updatedItem) {
+            toggleItemChecked(updatedItem);
+        }
+    }
+
+    const findItemInList = (item: Item): Item | undefined => {
+        return listItems.value.find(
+            (itemInList:Item) => itemInList.title === item.title);
+    }
+
+    const toggleItemChecked = (item: Item): void => {
+        item.checked = !item.checked;
+    }
+
 </script>
 
 <template>
     <ul>
-        <ListItem :isChecked="false">Este es el contenido </ListItem>
+        <li :key="key" v-for="(item, key) in listItems">
+            <ListItem :isChecked="item.checked" @toggle="updateItem(item)">
+                {{item.title}}
+            </ListItem>
+        </li>
     </ul>
 </template>
 
-<style>
-
+<style scoped>
+    ul {
+        list-style: none;
+    }
+    li {
+        margin: 0.5rem 0;
+    }
 </style>
